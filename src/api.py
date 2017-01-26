8d219ab0854192036be571f8d070dbf162211e7c
StarsCluster <scox@renci.org>
2017-01-26 15:32:53 -0500

    repurpose v2

--- src/api.py
+++ src/api.py
@@ -63,6 +63,44 @@ def repurpose (root, triplestore, word_embedding_path, threshold=0.8):
             visits[key_1] = 1
             visits[key_2] = 1
 
+
+from collections import defaultdict
+
+class GeneDrug(object):
+
+    def __init__(self, threshold=0.4):
+        self.gene_drug = defaultdict(list)
+        self.drug_gene = defaultdict(list)
+        self.threshold = threshold
+
+    def add (gene, drug):
+        self.gene_drug[gene].append (drug)
+        self.drug_gene[drug].append (gene)
+        '''
+        if gene in self.gene_drug:
+            drugs = self.gene_drug[gene]
+            drugs.append (drug)
+        else:
+            self.gene_drug[gene] = [ drug ]
+        '''
+    def get_target_similarity (self, L_drug, R_drug, model):
+        similarity = 0
+        if L_drug in self.drug_gene and R_drug in self.drug_gene:
+            L_genes = self.drug_gene[L_drug]
+            R_genes = self.drug_gene[R_drug]
+            visits = {}
+            for L in L_genes:
+                for R in R_genes:
+                    if L in model.vocab and R in model.vocab:
+                        key_1 = "{0}.{1}".format (L, R)
+                        key_2 = "{0}.{1}".format (R, L)
+                        if key_1 not in visits and key_2 not in visits:
+                            similarity = similarity + model.similarity (L, R)
+                            visits[key_1] = 1
+                            visits[key_2] = 1
+            similarity = similarity / len(visits) if len(visits) > 0 else similarity
+        return similarity
+
 def calculate_applicability (drugs_a, drugs_b, model):
     result = 0
     similarity = 0
@@ -84,26 +122,32 @@ def calculate_applicability (drugs_a, drugs_b, model):
     return result, similarity
 
 class RepurposeV2(object):
+
     def __init__(self, triplestore_uri, root, word_embedding_path, threshold=0.4):
         self.triplestore = TripleStore (triplestore_uri)
         self.root = root
         self.word_embedding_path = word_embedding_path
         self.threshold = threshold
+
     def run (self):
         query_file = os.path.join (self.root, "src", "query", "drug-indication.sparql")
         result = triplestore.execute_query (query_file)
         indications = {}
+        gene_drug = GeneDrug ()
         for binding in result['results']['bindings']:
             drug = binding['name']['value'].rsplit('/', 1)[-1].lower ()
             indication = binding['indication']['value'].rsplit('/', 1)[-1].lower ()
+            gene = binding['gene']['value'].rsplit('/', 1)[-1].lower ()
             if not drug in indications:
                 indications[drug] = [ indication ] 
             else:
                 if not indication in indications[drug]:
                     indications[drug].append(indication)
+            gene_drug.add (gene, drug)
 
         print ("loading word embedding model: {0}".format (word_embedding_path))
         model = word2vec.Word2Vec.load (word_embedding_path)
+        novel_indications = {}
         for ref_drug in indications:
             for other_drug in indications:
                 if ref_drug not in model.vocab or other_drug not in model.vocab:
@@ -114,7 +158,21 @@ class RepurposeV2(object):
                     other_indications = indications[other_drug]
                     for indication in other_indications:
                         if indication not in ref_indications:
-                            print ("drug {0} is similar to drug {1} and may treat {2}".format (other_drug, ref_drug, indication))
+                            if indication in novel_indications:
+                                if not other_drug in novel_indications[indication]:
+                                    novel_indications[indication].append (other_drug)
+                            else:
+                                novel_indications[indication] = [ other_drug ]
+
+        for indication in novel_indications:
+            ni = len (novel_indications[indication])
+            num_dk = 0
+            for drug in indications:
+                if indication in indications[drug]:
+                    num_dk = num_dk + 1
+            Sij = ni / num_dk
+            print ("indication => {0}: ni={1} num_dk={2} Sij={3} alt:{4}".format (indication, ni, num_dk, Sij, novel_indications[indication]))
+
 
 
 
